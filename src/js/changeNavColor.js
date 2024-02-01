84fe2a8fb033066b6a776fe4be4a72e4b5b4ac27
Simon <simon.breme@gmail.com>
2024-02-01 18:07:45 +0100

    Navigation Mobil und Desktop ready also changeColor

--- src/js/changeNavColor.js
+++ src/js/changeNavColor.js
@@ -2,45 +2,50 @@
 
 // Funktion, um die Farbe der Navigationselemente zu ändern
 function changeNavColor() {
-    var boxTopNav = document.querySelector('.Box_top-nav');
-    var LinkTopNav = document.querySelectorAll('.Link_top-nav');
-    var LinkTopNavMobile = document.querySelectorAll('.Link_top-nav-mobile');
-    var svgElement = document.querySelector('.Logo_top-nav');
-  
-    // Höhe des Headers plus Padding
-    var headerHeight = document.querySelector('header').offsetHeight -50;
-  
-    // Überprüfe, ob der Scrollwert größer als die Höhe des Headers ist
-    if (window.scrollY > headerHeight) {
-      // Ändere die Farbe auf Schwarz
-      LinkTopNav.forEach(function(item) {
-        item.style.color = 'black';
-      });
-  
-      LinkTopNavMobile.forEach(function(item) {
-        item.style.color = 'black';
-      });
-    // Ändere die SVG-Datei auf Schwarz
-    svgElement.innerHTML = '<use xlink:href="../images/Logo Black.svg"></use>';
-    
-    } else {
-      // Ändere die Farbe auf Weiß
-      LinkTopNav.forEach(function(item) {
-        item.style.color = 'white';
-      });
-  
-      LinkTopNavMobile.forEach(function(item) {
-        item.style.color = 'white';
-      });
+  var boxTopNav = document.querySelector(".Box_top-nav");
+  var LinkTopNav = document.querySelectorAll(".Link_top-nav");
+  var imgElement = document.querySelector(".Logo_top-nav");
+  var iconElement = document.querySelector(".top-nav__icon");
 
-      // Ändere die SVG-Datei auf Weiß
-      svgElement.innerHTML = '<use xlink:href="../images/Logo.svg"></use>';
-    }
+  var boxTopNavMobile = document.querySelector(".Menu_mobile-test");
+  var imgElementMobile = document.querySelector(".Logo_top-nav_mobile");
+  var iconElementMobile = document.querySelector(".Menu_toggle");
+
+  // Höhe des Headers plus Padding
+  var headerHeight = window.innerHeight * 0.60;
+
+  // Überprüfe, ob der Scrollwert größer als die Höhe des Headers ist
+  if (window.scrollY > headerHeight) {
+    // Ändere die Farbe auf Schwarz
+    LinkTopNav.forEach(function (item) {
+      item.style.color = "black";
+    });
+
+    // Ändere die SVG-Datei auf Schwarz
+    imgElement.src = "../images/Logo Black.svg";
+    iconElement.src = "../images/Shop Icon Black.svg";
+    imgElementMobile.src = "../images/Logo Black.svg";
+    iconElementMobile.src = "../images/Burger Menu Black.svg";
+
+  } else {
+    // Ändere die Farbe auf Weiß
+    LinkTopNav.forEach(function (item) {
+      item.style.color = "white";
+    });
+
+    // Ändere die SVG-Datei auf Weiß
+    imgElement.src = "../images/Logo.svg";
+    iconElement.src = "../images/Shop Icon White.svg";
+    imgElementMobile.src = "../images/Logo.svg";
+    iconElementMobile.src = "../images/Burger Menu.svg";
   }
-  
+}
+
+  // Rufe die Funktion changeNavColor auf, wenn das DOM vollständig geladen ist
+  document.addEventListener("DOMContentLoaded", function () {
   // Event-Listener für das Scroll-Event hinzufügen
-  document.addEventListener('scroll', changeNavColor);
-  
+  document.addEventListener("scroll", changeNavColor);
+
   // Initialen Aufruf der Funktion, um die Farbe beim Laden der Seite zu setzen
   changeNavColor();
-  
+});
