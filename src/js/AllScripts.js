ce5475b227cf405f8735b5fdf6e7d3d6be9136d7
Simon <simon.breme@gmail.com>
2024-02-06 17:17:45 +0100

    Links sind ready!

---
+++ src/js/AllScripts.js
@@ -0,0 +1,19 @@
+// Importieren Sie alle Skripte
+import myModule from './myModule.js';
+import index from './index.js';
+import changeNavColor from './changeNavColor.js';
+import changeNavColorMobil from './changeNavColorMobil.js';
+import currentPage from './currentPage.js';
+import script from './script.js';
+// Importieren Sie weitere Skripte, falls vorhanden
+
+// Exportieren Sie alle importierten Skripte
+export {
+    myModule,
+    index,
+    changeNavColor,
+    changeNavColorMobil,
+    currentPage,
+    script
+  // Exportieren Sie weitere Skripte, falls vorhanden
+};
